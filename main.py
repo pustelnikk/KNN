b9d40eee9078bc7770227fa902b179a59dbe4d4a
s22674 <s22674@pjwstk.edu.pl>
2022-03-17 17:26:03 +0100

    Implementacja otwierania CSV i znajdywania wielkosci wektora

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-
+import sys
 import csv
 
+def printList(list):
+    for entry in list:
+        print(entry)
+def findFloats(list):
+    size = 0
+    
+    for val in list:
+        try:
+            float(val)
+            size +=1
+        except:
+            pass
+    return size
+
 def main():
-    print("test")
 
+    if(len(sys.argv) < 4):
+        sys.exit("Brak argumentow")
 
+    k = sys.argv[1]
+    train = sys.argv[2]
+    test = sys.argv[3]
+    trainList = []
+    vectorSize = 0;
 
+    with open(train) as file:
+        reader = csv.reader(file, delimiter=",")
+
+        for row in reader:
+            trainList.append(row)
+
+    
+    #printList(trainList)
+    vectorSize = findFloats(trainList[0])
+    #print(vectorSize)
+
+    
 if __name__ == '__main__' :
     main()
